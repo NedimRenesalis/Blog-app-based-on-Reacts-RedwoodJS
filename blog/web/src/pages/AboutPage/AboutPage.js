f9eabc78140ccf4282fadb6cea2532b7263067e4
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-04 16:06:52 +0200

    Update AboutPage.js

--- blog/web/src/pages/AboutPage/AboutPage.js
+++ blog/web/src/pages/AboutPage/AboutPage.js
@@ -1,26 +1,11 @@
 import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const AboutPage = () => {
   return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Blog application</Link>
-        </h1>
-
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <p>This page tells you about stuff!</p>
-      </main>
-    </>
+    <BlogLayout>
+      <p>This page tells you about stuff!</p>
+    </BlogLayout>
   )
 }
 
