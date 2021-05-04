ab88bd59842e8dc07a5a60894f731d6a07512f41
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-04 16:03:30 +0200

    Update Bloglayout.js

--- blog/web/src/layouts/BlogLayout/BlogLayout.js
+++ blog/web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,5 +1,29 @@
+import { Link, routes } from '@redwoodjs/router'
+
 const BlogLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Blog application</Link>
+        </h1>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>{children}</main>
+
+      <footer>
+        ...
+      </footer>
+    </>
+  )
 }
 
 export default BlogLayout
