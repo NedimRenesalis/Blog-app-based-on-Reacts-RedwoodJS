afd5ce88819e94e4b94e2128d17ce651cf170b85
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-04 16:05:51 +0200

    Update HomePage.js

--- blog/web/src/pages/HomePage/HomePage.js
+++ blog/web/src/pages/HomePage/HomePage.js
@@ -1,25 +1,12 @@
 import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
 
 const HomePage = () => {
   return (
     <>
-      <header>
-        <h1>Blog application</h1>
-
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>This page is the home!</main>
-
-      <footer>
-        ...
-      </footer>
+      <BlogLayout>
+        <p>This page is the home!</p>
+      </BlogLayout>
     </>
   )
 }
